8659cbd211269155425847bda093f9e206588f96
lamperouge <825955095@qq.com>
2020-07-24 15:55:34 +0800

    test

--- src/GitTest2.java
+++ src/GitTest2.java
@@ -2,7 +2,7 @@ public class GitTest2 {
 
         public static void main(String arg[]) {
 
-            System.out.println("Git Test   " + "   2020/7/24" + "   test commit");
+            System.out.println("Git Test   " + "   2020/7/24" + "   test commit 2");
         }
 
 
